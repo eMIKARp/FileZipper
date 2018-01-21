3150c8db49723febc5be2065fd66a11a7672af2a
eMIKARp <emil.karpowicz@gmail.com>
2018-01-21 10:39:54 +0100

    Final version of FileZipper GUI

--- src/filezipper/Main.java
+++ src/filezipper/Main.java
@@ -11,9 +11,7 @@ public class Main extends JFrame
     private JButton bRemove;
     private JButton bZip;
     
-    JPanel mainPanel = new JPanel();
-    JPanel buttonPanel = new JPanel();
-    private JList listaPlikow = new JList();
+    private JList listaPlikow = new JList(new String[] {"Emil","Paulina","Karolina","Jagoda","Janusz"});
     
     private JMenuBar mainMenuBar = new JMenuBar();
 
@@ -22,12 +20,10 @@ public class Main extends JFrame
     {
         this.setTitle("FileZipper");
         this.setBounds(300,300,300,300);
-        this.getContentPane().add(mainMenuBar);
-        this.getContentPane().add(mainPanel);
-        
-        Action akcjaDodawania = new Akcja("Dodaj") {};
-        Action akcjaUsuwania = new Akcja("Usun") {};
-        Action akcjaZipowania = new Akcja("Zip") {};
+            
+        Action akcjaDodawania = new Akcja("Dodaj", "Dodaj nowy wpis do archiwum", "ctrl D", new ImageIcon("dodaj.png"));
+        Action akcjaUsuwania = new Akcja("Usuń", "Usuń zaznaczone wpisy z archiwum", "ctrl U", new ImageIcon("usun.png"));
+        Action akcjaZipowania = new Akcja("Zip", "Archiwizuj zaznaczone wpisy", "ctrl Z");
         
         bAdd = new JButton(akcjaDodawania);
         bRemove = new JButton(akcjaUsuwania);
@@ -40,23 +36,33 @@ public class Main extends JFrame
         JMenuItem menuUsun = menuPlik.add(akcjaUsuwania);
         JMenuItem menuZip = menuPlik.add(akcjaZipowania);
         
+        listaPlikow.setBorder(BorderFactory.createEtchedBorder());
+        
         GroupLayout layout = new GroupLayout(this.getContentPane());
         layout.setAutoCreateContainerGaps(true);
         layout.setAutoCreateGaps(true);
         
             layout.setHorizontalGroup(
                     layout.createSequentialGroup()
-                    .addComponent(listaPlikow)
+                    .addComponent(listaPlikow,100,150, Short.MAX_VALUE)
+                    .addContainerGap(10, Short.MAX_VALUE)
                     .addGroup(
-                    layout.createParallelGroup().addComponent(bAdd).addComponent(bRemove).addComponent(bZip)
+                    layout.createParallelGroup()
+                            .addComponent(bAdd)
+                            .addComponent(bRemove)
+                            .addComponent(bZip)
                     )
             );
             
             layout.setVerticalGroup(
                     layout.createParallelGroup()
-                        .addComponent(listaPlikow)
+                        .addComponent(listaPlikow, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(
-                        layout.createParallelGroup().addComponent(bAdd).addComponent(bRemove).addComponent(bZip)
+                        layout.createSequentialGroup()
+                                .addComponent(bAdd)
+                                .addComponent(bRemove)
+                                .addGap(10, 40, Short.MAX_VALUE)
+                                .addComponent(bZip)
                         )
             );
         
@@ -68,19 +74,33 @@ public class Main extends JFrame
     
     private class Akcja extends AbstractAction
     {
-        public Akcja (String nazwa)
+        public Akcja (String nazwa, String opis, String klawiaturowySkrot)
         {
             this.putValue(Action.NAME, nazwa);
+            this.putValue(Action.SHORT_DESCRIPTION, opis);
+            this.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(klawiaturowySkrot));
+          
+        }
+        
+        public Akcja (String nazwa, String opis, String klawiaturowySkrot, Icon ikona)
+        {
+            this(nazwa, opis, klawiaturowySkrot);
+            this.putValue(Action.SMALL_ICON, ikona);
         }
 
         @Override
-        public void actionPerformed(ActionEvent e) {
-            
+        public void actionPerformed(ActionEvent e) 
+        {
+            if (e.getActionCommand().equals("Dodaj"))
+                System.out.println("Dodawanie");
+            else if (e.getActionCommand().equals("Usuń"))
+                System.out.println("Usuwanie");
+            else if (e.getActionCommand().equals("Zip"))
+                System.out.println("Archiwizacja");
         }
         
     }
-    
-    
+
     
     public static void main(String[] args) 
     {
