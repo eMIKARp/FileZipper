6125f1dafe4fcbd460e59d83a305675741bd479a
eMIKARp <emil.karpowicz@gmail.com>
2018-01-21 20:46:56 +0100

    Adding 'archive files' functionality. Application is finished.

--- src/filezipper/Main.java
+++ src/filezipper/Main.java
@@ -3,8 +3,15 @@ package filezipper;
     import javax.swing.*;
     import java.awt.*;
     import java.awt.event.*;
-import java.io.File;
-import java.util.ArrayList;
+    import java.io.BufferedInputStream;
+    import java.io.BufferedOutputStream;
+    import java.io.File;
+    import java.io.FileInputStream;
+    import java.io.FileOutputStream;
+    import java.io.IOException;
+    import java.util.ArrayList;
+    import java.util.zip.ZipEntry;
+    import java.util.zip.ZipOutputStream;
 
 
 public class Main extends JFrame 
@@ -37,7 +44,6 @@ public class Main extends JFrame
         return super.remove(index);  
             
         }
-        
 
         ArrayList lista = new ArrayList();
             
@@ -47,7 +53,8 @@ public class Main extends JFrame
     private JScrollPane listaPrzewijana = new JScrollPane(listaPlikow);
     private JMenuBar mainMenuBar = new JMenuBar();
     private JFileChooser fileChooser = new JFileChooser();
-    
+    public static final int BUFFOR = 1024;
+    ArrayList listaSciezek = new ArrayList();
     
     public Main()
     {
@@ -129,7 +136,7 @@ public class Main extends JFrame
             else if (e.getActionCommand().equals("Usuń"))
                 usunWpisyZArchiwum();
             else if (e.getActionCommand().equals("Zip"))
-                System.out.println("Archiwizacja");
+                stworzenieArchiwumZip();
         }
         
     }
@@ -148,7 +155,6 @@ public class Main extends JFrame
                     if (!czyWpisSiePowtarza(filesToArchive[i].getPath()))
                         modelListy.addElement(filesToArchive[i]);
             }
-            
     }
     
     private void usunWpisyZArchiwum()
@@ -160,6 +166,73 @@ public class Main extends JFrame
             modelListy.remove(tmp[i]-i);
         }
     }
+    
+    private void stworzenieArchiwumZip()
+    {    
+        
+        fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+        fileChooser.setSelectedFile(new File(System.getProperty("user.dir")+ File.separator +"NewZipFile.zip"));
+        int tmp = fileChooser.showDialog(rootPane, "Kompresuj");
+        
+        if (tmp == JFileChooser.APPROVE_OPTION)
+        {
+            byte tmpData[] = new byte[BUFFOR];
+
+            try
+            {
+                ZipOutputStream zOutS = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(fileChooser.getSelectedFile()), BUFFOR));
+                for (int i = 0; i < modelListy.getSize(); i++)
+                {
+                    if (!((File)modelListy.get(i)).isDirectory())
+                        zipuj(zOutS, (File)modelListy.get(i), tmpData, ((File)modelListy.get(i)).getPath());
+                    else
+                    {
+                        wypiszSciezki((File)modelListy.get(i));
+                        for (int j = 0; j < listaSciezek.size(); j++)
+                        {
+                            zipuj(zOutS, (File)listaSciezek.get(j), tmpData, ((File)modelListy.get(i)).getPath());
+                        }
+                        listaSciezek.removeAll(listaSciezek);
+                    }
+                        
+                }
+                
+                zOutS.close();
+            }
+            catch (IOException ex)
+            {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    
+    
+    private void zipuj(ZipOutputStream zOutS, File sciezkaPliku, byte[] tmpData, String sciezkaBazowa) throws IOException
+    {
+        
+        BufferedInputStream inS = new BufferedInputStream(new FileInputStream(sciezkaPliku), BUFFOR);
+        zOutS.putNextEntry(new ZipEntry(sciezkaPliku.getPath().substring(sciezkaBazowa.lastIndexOf(File.separator))));
+        int counter;
+        while ((counter = inS.read(tmpData, 0, BUFFOR)) != -1)
+        zOutS.write(tmpData, 0, counter);
+        zOutS.closeEntry();
+        inS.close();
+        
+    }
+    
+    private void wypiszSciezki (File nazwaSciezki)
+    {
+        String[] nazwyPlikowIKatalogow = nazwaSciezki.list();
+        for (int i = 0; i < nazwyPlikowIKatalogow.length; i++)
+            {
+                File p = new File(nazwaSciezki.getPath(), nazwyPlikowIKatalogow[i]);
+                if (p.isFile())
+                    listaSciezek.add(p);
+                if (p.isDirectory())
+                   wypiszSciezki(new File(p.getPath()));
+            }
+    }
+    
     
     private boolean czyWpisSiePowtarza(String testowanyWpis)
     {
